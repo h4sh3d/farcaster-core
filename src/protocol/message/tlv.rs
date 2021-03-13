9bcc2511209dcff659e5ce50a29a3626abb26f9a
h4sh3d <h4sh3d@protonmail.com>
2021-03-13 11:45:40 +0100

    Does no compile, try tlv from lnpbp

--- src/protocol/message/tlv.rs
+++ src/protocol/message/tlv.rs
@@ -1,7 +1,16 @@
-
-#[derive(Clone, Debug, Api)] //
+#[derive(Clone, Debug, Api)]
 #[api(encoding = "lightning")]
 pub enum Message {
+    #[api(type = 0x0001)]
+    Hello(String),
+
+    /// Some attribute
+    #[api(type = 0x0003)]
+    Empty(),
+
+    #[api(type = 0x0005)]
+    NoArgs,
+
     #[api(type = 0x0103)]
-    AddKeys(Vec<u8>),
+    AddKeys(u8),
 }
